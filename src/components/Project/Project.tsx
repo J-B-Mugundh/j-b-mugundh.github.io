b5b164212081c2afca21b51e4536b706d615ee93
Mugundh <mugundhjb@gmail.com>
2024-02-11 23:34:24 +0530

    Updated projects

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -19,20 +19,20 @@ export function Project() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links center-image">
-                <a href="https://github.com/J-B-Mugundh/E-MEDICAL-SERVICE-PLATFORM" target="_blank" rel="noreferrer">
+                <a href="https://github.com/J-B-Mugundh/Next-Gen-Security_Enhanced-DDoS-Attack-Detection-for-Autonomous-Vehicles-in-6G-Networks" target="_blank" rel="noreferrer">
                   <img src={githubIcon} style={{float: "right"}} alt="Visit site" />
                 </a>
               </div>
             </header>
             <div className="body">
-              <h3>E-Medical Service Platform</h3>
+              <h3>Next-Gen Security: Enhanced DDoS Attack Detection for AVs in 6G Networks</h3>
               <p>
-              An impactful E-Medical Services Platform specifically designed to provide essential healthcare services during pandemic times.
+              This research proposed an Advanced DDoS Attack Detection System using a Hybrid Detection Model (HDM) and Continuous Learning Model (CLM) for dynamic adaptation to evolving attack patterns.
               </p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li>HTML, CSS, JavaScript, Bootstrap, PHP, MySQL</li>
+                <li>Python, Machine Learning, SUMO</li>
               </ul>
             </footer>
           </div>
@@ -43,20 +43,20 @@ export function Project() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links center-image">
-                <a href="https://github.com/J-B-Mugundh/E-MEDICAL-SERVICE-PLATFORM" target="_blank" rel="noreferrer">
+                <a href="https://github.com/J-B-Mugundh/Integrated-Assistive-System-For-Object-Detection-And-Blind-Navigation" target="_blank" rel="noreferrer">
                   <img src={githubIcon} style={{float: "right"}} alt="Visit site" />
                 </a>
               </div>
             </header>
             <div className="body">
-              <h3>E-Medical Service Platform</h3>
+              <h3>Integrated Assistive System For Object Detection And Blind Navigation</h3>
               <p>
-              An impactful E-Medical Services Platform specifically designed to provide essential healthcare services during pandemic times.
+              Integrated assistive system combining advanced object detection and blind navigation, designed to enhance accessibility for individuals with visual impairments.
               </p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li>HTML, CSS, JavaScript, Bootstrap, PHP, MySQL</li>
+                <li>Python, OpenCV, Yolo, Raspberry Pi</li>
               </ul>
             </footer>
           </div>
@@ -67,20 +67,20 @@ export function Project() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links center-image">
-                <a href="https://github.com/J-B-Mugundh/E-MEDICAL-SERVICE-PLATFORM" target="_blank" rel="noreferrer">
+                <a href="https://github.com/J-B-Mugundh/File-Locking-Mechanism-Using-Facial-Recognition" target="_blank" rel="noreferrer">
                   <img src={githubIcon} style={{float: "right"}} alt="Visit site" />
                 </a>
               </div>
             </header>
             <div className="body">
-              <h3>E-Medical Service Platform</h3>
+              <h3>FileCoffer - File Locking Mechanism Using Facial Recognition</h3>
               <p>
-              An impactful E-Medical Services Platform specifically designed to provide essential healthcare services during pandemic times.
+              An innovative approach to enhance file protection and deception detection mechanisms using face recognition technology within the Windows operating system.
               </p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li>HTML, CSS, JavaScript, Bootstrap, PHP, MySQL</li>
+                <li>Python, OpenCV, Machine Vision, OS</li>
               </ul>
             </footer>
           </div>
